dfc83849746baa90fe36670ec5c72259e84a46b7
brianarmendariz <brian.armendariz@student.csulb.edu>
2015-11-07 19:01:08 -0800

    Create Rule.java
    Rule for the parse matrix

---
+++ Rule.java
@@ -0,0 +1,16 @@
+package com.csulb.compiler;
+
+public class Rule {
+	
+	private String description;
+	
+	public Rule(String d)
+	{
+		description = d;
+	}
+	
+	public String getRule()
+	{
+		return description;
+	}
+}
