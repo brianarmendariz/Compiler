77cb3938bb7fc0d3e3218b5420e44333b7c3e49d
brianarmendariz <brian.armendariz@student.csulb.edu>
2015-11-07 19:00:18 -0800

    Update Matrix.java
    new matrix including a cleaner matrix consisting of integers. these numbers then reference a number in the ruleArray which is an array of Rule objects

--- Matrix.java
+++ Matrix.java
@@ -2,58 +2,83 @@ package com.csulb.compiler;
 
 public class Matrix {
 	
-	private String[][] myMatrix = { 
-			{"prog brace1 STMTS brace2", "error", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "error", "eps"},
+	private int[][] myMatrix = { 
+			{1, 0, 0, 0, 0, 0, //1
+			 0, 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0, 19},
 			 
-			{"error", "STMT semi STMTS", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "error", "error", 
-			 "error", "eps", "STMT semi STMTS", "error", "error", "error"},
+			{0, 2, 0, 0, 0, 0, 
+			 0, 0, 0, 0, 0, 0,
+			 0, 19, 2, 0, 0, 0},
+			
+			{0, 3, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0, 0,
+			 0, 0, 4, 0, 0, 0},
+			
+			{0, 0, 5, 0, 0, 0,
+			 0, 0, 0, 0, 6, 0,
+			 0, 0, 6, 6, 6, 0},
 			 
-			{"error", "print parens1 ELIST parens2", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "error", "error", 
-			 "error", "error", "id equal Y", "error", "error", "error"},
+			{0, 0, 0, 0, 0, 0, 
+		     0, 0, 0, 0, 7, 19,
+		     0, 0, 7, 7, 7, 0},
 			 
-			{"error", "error", "input", "error", "error", "error", 
-			 "error", "error", "error", "error", "E", "error", 
-			 "error", "error", "E", "E", "E", "error"},
+			{0, 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 8, 0,
+			 0, 0, 8, 8, 8, 0},
 			 
-			{"error", "error", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "E comma ELIST", "eps", 
-			 "error", "error", "E comma ELIST", "E comma ELIST", "E comma ELIST", "error"},
-			 
-			{"error", "error", "error", "error", "error", "error",
-			 "error", "error", "error", "error", "T Q", "error", 
-			 "error", "error", "T Q", "T Q", "T Q", "error"},
+			{0, 0, 0, 9, 10, 0,
+		     0, 0, 19, 19, 0, 19,
+		     0, 0, 0, 0, 0, 0},
 
-			{"error", "error", "error", "plus T Q", "minus T Q", "error", 
-			 "error", "error", "eps", "eps", "error", "eps", 
-			 "error", "error", "error", "error", "error", "error"},		
+			{0, 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 11, 0,
+			 0, 0, 11, 11, 11, 0},
+			
+			{0, 0, 0, 9, 10, 12,
+		     13, 14, 19, 19, 0, 19, 
+		     0, 0, 0, 0, 0, 0},
 
-			{"error", "error", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "F R", "error", 
-			 "error", "error", "F R", "F R", "F R", "error"},
-			 
-			{"error", "error", "error", "plus T Q", "minus T Q", "aster F R", 
-			 "slash F R", "caret F R", "eps", "eps", "error", "eps", 
-			 "error", "error", "error", "error", "error", "error"},
-			 
-			{"error", "error", "error", "error", "error", "error", 
-			 "error", "error", "error", "error", "parens1 E parens2", "error", 
-			 "error", "error", "id", "num", "string", "error"},
+			{0, 0, 0, 0, 0, 0,
+		     0, 0, 0, 0, 15, 0, 
+		     0, 0, 16, 17, 18, 0}
 		};
 	
 	String[] nonTerminalArray = { "PGM", "STMTS", "STMT", "Y", "ELIST", "E", "Q", "T", "R", "F" } ;
 	String [] terminalArray = { "prog", "print", "input", "plus", "minus", "aster", "slash", "caret", 
 			"comma", "semi", "parens1", "parens2", "brace1", "brace2", "id", "num", "string", "EOF" };
 	
+	private Rule rule0 = new Rule("error");
+	private Rule rule1 = new Rule("prog brace1 STMTS brace2");
+	private Rule rule2 = new Rule("STMT semi STMTS");
+	private Rule rule3 = new Rule("print parens1 ELIST parens2");
+	private Rule rule4 = new Rule("id equal Y");
+	private Rule rule5 = new Rule("input");
+	private Rule rule6 = new Rule("E");
+	private Rule rule7 = new Rule("E comma ELIST");
+	private Rule rule8 = new Rule("T Q");
+	private Rule rule9 = new Rule("plus T Q");
+	private Rule rule10 = new Rule("minus T Q");
+	private Rule rule11 = new Rule("F R");
+	private Rule rule12 = new Rule("aster T Q");
+	private Rule rule13 = new Rule("slash F R");
+	private Rule rule14 = new Rule("caret F R");
+	private Rule rule15 = new Rule("parens1 E parens2");
+	private Rule rule16 = new Rule("id");
+	private Rule rule17 = new Rule("num");
+	private Rule rule18 = new Rule("string");
+	private Rule rule19 = new Rule("eps");
+	
+	Rule[] ruleArray = { rule0, rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8,
+			rule9, rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, 
+			rule18, rule19 };
+	
 	public Matrix() 
 	{
-		
+
 	}
 	
-	public String[][] getMatrix()
+	public int[][] getMatrix()
 	{
 		return myMatrix;
 	}
@@ -98,17 +123,17 @@ public class Matrix {
 	{
 		int rowNon = getIndexOfNonTerminal(nonTerminal);
 		int colTerm = getIndexOfTerminal(terminal);
-		String rule = "";
+		int ruleNum = 0;
 				
 		if(rowNon < 0 || colTerm < 0 || rowNon > 10 || colTerm > 18)
 		{
-			return "NO RULE";
+			return ruleArray[ruleNum].getRule();
 		}
 		else 
 		{
-			rule = myMatrix[rowNon][colTerm];
+			ruleNum = myMatrix[rowNon][colTerm];
 		}
-		return rule;
+		return ruleArray[ruleNum].getRule();
 	}
 	
 	public String toString()
