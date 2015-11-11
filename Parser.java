db35a8edf17daa14aed787aab16169ec0a397735
brianarmendariz <brian.armendariz@student.csulb.edu>
2015-11-10 16:44:42 -0800

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -7,6 +7,8 @@ public class Parser {
 	Lexer m_lex;
 	Stack<String> m_stack;
 	Matrix m_matrix;
+	String[] m_endTokens = { "semi", "brace2", "EOF" };
+	boolean errors = false;
 	
 	public Parser(String inputFile) 
 	{
@@ -15,10 +17,11 @@ public class Parser {
 		m_matrix = new Matrix();
 	}
 	
-	public void parse() throws Exception
+	public void parse() //throws Exception
 	{
 		m_stack.push("EOF");
 		m_stack.push("PGM");
+	
 		
 		Token token = m_lex.nextToken();
 		String tokenType = token.getWord();
@@ -31,66 +34,129 @@ public class Parser {
 			
 			System.out.println("\nprediction = " + prediction + ", token = " + tokenType + ", data = " + data );
 			
-			
-			
-			if(m_matrix.isNonTerminal(prediction)) 
+			if(tokenType.equals(prediction))
+			{
+					System.out.println("MATCH: [" + tokenType + "]");
+					token = m_lex.nextToken();
+					tokenType = token.getWord();
+					data = token.getData();
+					System.out.println("tokenType = " + tokenType);
+			}
+			else if(m_matrix.isNonTerminal(prediction)) 
 			{
 				String rule = m_matrix.getRule(prediction, tokenType);
 				System.out.println("rule = " + rule);
-				if("NO RULE".equals(rule))
-				{
-					throw new Exception("Error: at " + token.getLineNumber() + " at Expected " 
-							+ prediction + " but saw " + tokenType);
+				if("error".equals(rule))
+				{	
+					try {
+						errorRecovery(prediction, token);
+					} catch (Exception e) {
+						e.printStackTrace();
+						errors = true;
+					}
+					token = m_lex.backupToken();
+					tokenType = token.getWord();
+					data = token.getData();
+					System.out.println("token = " + token.getWord());
 				}
 				else {
 					pushTheRule(rule);
 				}
 			}
-			else if(m_matrix.isTerminal(prediction))
-			{
-				if(!prediction.equals(tokenType))
-				{
-					throw new Exception("Error: at " + token.getLineNumber() + " at Expected " 
-							+ prediction + " but saw " + tokenType);
-				}
-				else 
-				{
-					System.out.println("MATCH: [" + tokenType + "]");
-					token = m_lex.nextToken();
-					tokenType = token.getWord();
-					data = token.getData();
-				}
-			}
-			else if(prediction.equals("equal") && tokenType.equals("equal"))
-			{ // if it's an equal sign then eat it
-				token = m_lex.nextToken();
-				tokenType = token.getWord();
-				data = token.getData();
-			}
 			else if(prediction.equals("eps"))
 			{
 				// if epsilon then continue
+			}			
+			else if(!prediction.equals(tokenType))
+			{	
+				try {
+					errorRecovery(prediction, token);
+				} catch (Exception e) {
+					e.printStackTrace();
+					errors = true;
+				}
+				token = m_lex.backupToken();
+				tokenType = token.getWord();
+				data = token.getData();
+				System.out.println("token = " + token.getWord());
 			}
+			
 			if(tokenType.equals("EOF"))
 			{
 				break;
-			}
+			}			
 			System.out.println("..................................");
-			for(String st : m_stack) 
-			{
-				System.out.println(st);
+			System.out.println("...top of stack...");
+			for (int i = m_stack.size() - 1; i >= 0; i--) {
+			    System.out.println("\t" + m_stack.get(i));
 			}
+			System.out.println("...bottom of stack...");
 		}
 
-		if(tokenType.equals("EOF"))
+		StringBuilder sb = new StringBuilder("\nParse is COMPLETE: ");
+		if(tokenType.equals("EOF") && !errors)
 		{
-			System.out.println("\nParse is COMPLETE");
+			sb.append("you have no errors");
 		}
 		else 
 		{
-			System.out.println("\nYou need to DEBUG");
+			sb.append("you have ERRORS");
 		}
+		System.out.println(sb.toString());
+	}
+	
+	public void errorRecovery(String prediction, Token token) throws Exception
+	{
+		//output original error message
+		try
+		{
+			throw new Exception("Error: at " + token.getLineNumber() + " at Expected " 
+					+ prediction + " but saw " + token.getWord());
+		}
+		finally
+		{
+			//pop stack until an end token is seen
+			boolean found  = false;
+			while(!found)
+			{
+				for(int i = 0; i < m_endTokens.length; i++)
+				{
+					System.out.println("m_endTokens[i] = " + m_endTokens[i] +
+							", prediction = " + prediction);
+					
+					if(m_endTokens[i].equals(prediction))
+					{
+						found = true;
+						break;
+					}
+					
+					if(!found && i == m_endTokens.length - 1)
+					{
+						m_stack.pop();
+					}
+				}
+				//get the next token until it is an end token
+				if(!m_stack.isEmpty())
+				{
+					prediction = m_stack.peek();
+				}
+				//if recovery cannot be made let user know
+				if(!found && m_stack.isEmpty())
+				{
+					throw new Exception("Error: UNRECOVERABLE, critical damage ");
+				}
+			}
 
+			while(m_lex.hasNextToken() && !prediction.equals(token.getWord()))
+			{
+				System.out.println("token.getWord() = " + token.getWord() +
+						", prediction = " + prediction);
+				token = m_lex.nextToken();
+			}
+			System.out.println("token.getWord() = " + token.getWord());
+			System.out.println("Skipping to the end of the <program-construct> at line " +
+					token.getLineNumber());	
+		}
 	}
 	
 	public void pushTheRule(String rule)
@@ -121,7 +187,7 @@ public class Parser {
 	{
 		while(!m_stack.isEmpty())
 		{
-			System.out.println(m_stack.pop());
+			System.out.println(m_stack.peek());
 		}
 	}
 	
