cd2ed2d85ef3dba0f76ad181e9663b5620116db8
brianarmendariz <brian.armendariz@student.csulb.edu>
2015-11-19 15:11:49 -0800

    Create Symbol.java

---
+++ Symbol.java
@@ -0,0 +1,16 @@
+package com.csulb.compiler;
+
+public class Symbol {
+	
+	private String data;
+	
+	public Symbol(String d)
+	{
+		data = d;
+	}
+	
+	public String getData()
+	{
+		return data;
+	}
+}
